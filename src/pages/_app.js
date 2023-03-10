c2cc1959eb3234f434162687ea0d8398a898391c
Parimal2607 <104558536+Parimal2607@users.noreply.github.com>
2023-03-10 16:43:05 +0530

    update title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import '@/styles/globals.css'
 import "../assets/scss/style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Parimal Sharma</title>
+      </Head>
+  <Component {...pageProps} />
+    </>
+  )
 }
